344dd11535ee9ec66cd26f4e8d39dad525c7e404
ningfei.li <ningfei.li@gloryholiday.com>
2018-12-13 22:52:51 +0800

    sum solution

---
+++ leet_code/src/main/java/两数之和/App.java
@@ -0,0 +1,67 @@
+/*
+ * App.java  * Created on  2018/12/13
+ * Copyright (c) 2018 YueTu
+ * YueTu TECHNOLOGY CO.,LTD. All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * YueTu Ltd. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement you
+ * entered into with YueTu Ltd.
+ */
+package 两数之和;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author ling.zhang
+ */
+public class App {
+
+    /*给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+    你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+
+    示例:
+
+    给定 nums = [2, 7, 11, 15], target = 9
+
+    因为 nums[0] + nums[1] = 2 + 7 = 9
+    所以返回 [0, 1]*/
+
+    public static void main(String[] args) {
+        int[] elements = {2,5,5,11};
+        int target = 7;
+        int[] result = twoSumTwoHash(elements,target);
+        System.out.println(result);
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for(int i=0;i<nums.length;i++){
+            for(int j=1;j<nums.length;j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("no two sum solution");
+    }
+
+
+    public static int[] twoSumTwoHash(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+
+}
